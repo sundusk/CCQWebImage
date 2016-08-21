ea23ed6e9806cf7876191e70bc408605bffbefd4
sundusk <467941935@qq.com>
2016-08-21 15:23:10 +0800

    提交项目

---
+++ CCQSD/CCQSD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCQSD
+//
+//  Created by 神威 on 16/8/21.
+//  Copyright © 2016年 ccq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
